9f4779b8424c955775e208325463fc614e5dca7b
OualiAghiles <ouali.aghiles@gmail.com>
2020-03-06 23:42:49 +0100

    add html plugins  starting class accordion

---
+++ source/assets/scripts/plugins/Accordion.js
@@ -0,0 +1,28 @@
+class Accordion {
+
+	constructor(className) {
+    this.els = document.querySelector(className)
+    this.accordions = Array.from(this.else)
+		this.accordions.forEach(accordion => {
+      this.toggle(accordion)
+    })
+  }
+  
+  this.show () {
+
+  }
+
+  this.hide () {
+
+  }
+
+  this.toggle(accordion) {
+    const headers = accordion.querySelectorAll('header')
+    headers.forEach(h => {
+      h.addEventListener('click', (e) {
+        
+      })
+    })
+
+  }
+}
